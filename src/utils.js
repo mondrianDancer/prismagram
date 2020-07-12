53e9445bf9b7f26142d8b9cf8938be27747f0993
MondrianDancer <mondrian.dance@gmail.com>
2020-07-12 15:41:51 +0900

    sendMail Function with Nodemailer & mailgun

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,25 @@ export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
+
+const sendMail = (email) => {
+  const api_key = process.env.APIKEY;
+  const domain = process.env.DOMAIN;
+  const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+
+  return mailgun.messages().send(email, (error, body) => {
+    if (error) console.log(error);
+    else console.log(body);
+  });
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "Juyeon@gmail.com",
+    to: adress,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret is ${secret}. <br/>
+          Copy paste on the app/website to login`,
+  };
+  return sendMail(email);
+};
